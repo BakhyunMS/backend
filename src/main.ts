c0005f8b1f8182280601c7fdf7ee417e126172d0
chul0721 <chul070721@naver.com>
2022-07-10 12:22:35 +0900

    feat: setup project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  await app.listen(3000)
 }
-bootstrap();
+
+PrismaClient.$connect().then(() => bootstrap())
