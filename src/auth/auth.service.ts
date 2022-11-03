8519f1ec1abd7d8afe6d0757b54d8dc31348c8a3
chul0721 <chul070721@naver.com>
2022-11-03 21:14:35 +0900

    fix: fix password compare error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -11,20 +11,16 @@ export class AuthService {
   constructor(private jwtService: JwtService, private configService: ConfigService) {}
 
   async login(email: string, password: string): Promise<LoginResponse> {
-    const existingUser = await prisma.user.findFirst({
+    const user = await prisma.user.findFirst({
       where: {
         email
       }
     })
-    const user = await prisma.user.findFirst({
-      where: {
-        email,
-        password: await bcrypt.hash(password, 10)
-      }
-    })
+    if (!user) return { ok: false, message: '존재하지 않는 이메일입니다.' }
 
-    if (!existingUser) return { ok: false, message: '존재하지 않는 이메일입니다.' }
-    if (!user) return { ok: false, message: '비밀번호가 일치하지 않습니다.' }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordCorrect) return { ok: false, message: '비밀번호가 일치하지 않습니다.' }
 
     const token = await this.jwtService.sign({ id: user.id }, this.configService.get('JWT_SECRET'))
 
